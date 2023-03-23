39c1992a9fa7a860fa542dc2f2861ee5e6b69e10
arnaudchampetier <arnaud.champetier9@gmail.com>
2023-03-23 17:05:45 +0100

    quete 8 interface

--- Eagle.java
+++ Eagle.java
@@ -21,4 +21,39 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+public void takeOff() {
+    if (!this.flying) {
+        System.out.println("Taking off...");
+        this.flying = true;
+    }
+}
+
+public void ascend(int meters) {
+    if (this.flying) {
+        System.out.println("Ascending to " + meters + " meters...");
+        this.altitude += meters;
+    }
+}
+
+public void glide() {
+    if (this.flying) {
+        System.out.println("Gliding...");
+    }
+}
+
+public void descend(int meters) {
+    if (this.flying) {
+        System.out.println("Descending to " + meters + " meters...");
+        this.altitude -= meters;
+    }
+}
+
+public void land() {
+    if (this.flying && this.altitude == 0) {
+        System.out.println("Landing...");
+        this.flying = false;
+    }
+}
+
 }
